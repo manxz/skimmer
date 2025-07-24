2760b45880406fa89f5bf103696464d2c3520551
Oscar Manxz <oscarmanxz@Manxz.local>
2025-07-23 22:03:14 -0700

    Force redeploy to update schema

--- index.js
+++ index.js
@@ -77,4 +77,4 @@ app.delete('/clients/:id', async (req, res) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-}); 
+}); // Force redeploy
